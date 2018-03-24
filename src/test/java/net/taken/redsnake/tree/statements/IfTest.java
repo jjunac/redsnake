91d031549351822da9928a9d7332602407e76e4d
Jeremy Junac <jeremy.junac@etu.unice.fr>
2018-03-24 11:41:36 +0100

    more tests for if closes#5

--- src/test/java/net/taken/redsnake/tree/statements/IfTest.java
+++ src/test/java/net/taken/redsnake/tree/statements/IfTest.java
@@ -4,6 +4,7 @@ import com.google.common.collect.ImmutableList;
 import net.taken.redsnake.lang.RedsBoolean;
 import net.taken.redsnake.lang.RedsEnvironment;
 import net.taken.redsnake.lang.RedsInteger;
+import net.taken.redsnake.tree.Program;
 import net.taken.redsnake.tree.Variable;
 import net.taken.redsnake.tree.statements.If;
 import net.taken.redsnake.tree.statements.StatementList;
@@ -14,8 +15,7 @@ import org.junit.jupiter.api.Test;
 
 import java.util.Optional;
 
-import static net.taken.redsnake.tree.TestUtils.mockExpression;
-import static net.taken.redsnake.tree.TestUtils.mockVariable;
+import static net.taken.redsnake.tree.TestUtils.*;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
@@ -64,6 +64,26 @@ class IfTest {
         If anIf = createIfWithAssignments("a", false, 1, 2);
         env.putVariable("a", new RedsInteger(0));
         anIf.execute(env);
+        assertEquals(new RedsInteger(2), env.getVariable("a"));
+    }
+
+    @Test
+    void shouldExecuteThenBlockWhenExecutingIfWithParenthesisAndCurlyBracketsAndNotInline() {
+        Program program = parseProgram("a = 0\n" +
+            "if(true) {\n" +
+            "    a = 1\n" +
+            "} else {\n" +
+            "    a = 2\n" +
+            "}");
+        program.execute(env);
+        assertEquals(new RedsInteger(1), env.getVariable("a"));
+    }
+
+    @Test
+    void shouldExecuteElseBlockWhenExecutingIfWithoutParenthesisAndWithoutCurlyBracketsAndInline() {
+        Program program = parseProgram("a = 0\n" +
+            "if false a = 1 else a = 2");
+        program.execute(env);
         assertEquals(new RedsInteger(2), env.getVariable("a"));
     }
 
